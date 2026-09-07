e5540471b5922230024cf0fd80193ec12724674f
ttjrbh467-pixel <ttjrbh467@gmail.com>
2026-09-08 00:34:06 +0300

    Add Firebase client authentication setup

---
+++ frontend/src/lib/firebase.ts
@@ -0,0 +1,31 @@
+import { Platform } from "react-native";
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth, initializeAuth, getReactNativePersistence, GoogleAuthProvider } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY as string,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID as string,
+};
+
+const firebaseApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+let firebaseAuth;
+if (Platform.OS === "web") {
+  firebaseAuth = getAuth(firebaseApp);
+} else {
+  try {
+    firebaseAuth = initializeAuth(firebaseApp, {
+      persistence: getReactNativePersistence(AsyncStorage),
+    });
+  } catch {
+    firebaseAuth = getAuth(firebaseApp);
+  }
+}
+
+export { firebaseAuth };
+export const googleProvider = new GoogleAuthProvider();
